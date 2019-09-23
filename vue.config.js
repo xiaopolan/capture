1966561cad620a133f007561eb005181286af8eb
xiaopolan <1027752452@qq.com>
2019-09-23 13:46:06 +0800

    提交

--- vue.config.js
+++ vue.config.js
@@ -13,19 +13,22 @@ var npmScriptsName = process.env.npm_lifecycle_event;
 var configObj = require('./build/base.config'); // 引入项目配置文件
 var portObj = configObj.port; // 端口号配置对象
 var apiUrlObj = configObj.apiUrl; // 接口地址配置对象
-console.log(portObj);
-console.log(apiUrlObj);
+// console.log(portObj);
+// console.log(apiUrlObj);
 var port = portObj[npmScriptsName]; // 端口号
 var apiUrl = apiUrlObj[npmScriptsName]; // 接口地址
-console.log(port);
-console.log(apiUrl);
+console.log("port"+port);
+console.log("apiUrl"+apiUrl);
+console.log("__dirname"+__dirname)
 //吴剑
-// var targeturl="http://192.168.10.100:8080"
+// var targeturl="http://192.168.10.44:8080"
   //亢玉涵
-  // var targeturl="http://192.168.10.63:8080"
+	// var targeturl="http://192.168.10.85:8081"
+	//生产
+	// var targeturl="http://auction.hz-huanshi.com"
   //支付通道
 // var pay='http://www.xxsrchjc.com/Pay_Index.html'
-//服务器
+//测试
 var targeturl="http://47.111.184.160:8080"
 module.exports = {
 	baseUrl: './',
@@ -42,7 +45,7 @@ module.exports = {
     			changeOrigin: true, // 如果接口跨域，需要进行这个参数配置为true
     			// 路径重写
     			pathRewrite: {
-    				// '/api': '/yh/ihr/api',  // 重写请求，比如我们源访问的包含/api，那么请求会将/api替换为/yh/ihr/api
+    				// '^/api': '/pro',  // 重写请求，比如我们源访问的包含/api，那么请求会将/api替换为/pro
     				'^/api': ''
     			}
     		},
@@ -80,7 +83,7 @@ module.exports = {
         ],
         /*
             externals：这个配置选项提供了「从输出的 bundle 中排除依赖」的方法。
-                防止将某些 import 的包(package)打包到 bundle 中，而是在运行时(runtime)再去从外部获取这些扩展依赖。
+                防止将某些 import 的包(package)打包到 b undle 中，而是在运行时(runtime)再去从外部获取这些扩展依赖。
                 例如，从CDN引入echarts，而不是把它打包。
         */
         externals: {
